429a8301be90e71c8e3160c9e23a3770b31c96fc
nikhil shakya <nikhil.shakya@roado.co.in>
2023-02-13 22:52:45 +0530

    minor updates

--- src/b2b/pages/OrderInvoices/AttractionInvoicePdfTemplate.jsx
+++ src/b2b/pages/OrderInvoices/AttractionInvoicePdfTemplate.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 
-const AttractionInvoicePdfTemplate = () => {
+const AttractionInvoicePdfTemplate = ({ data }) => {
+  console.log(data);
   return (
-    <div>
+    <div className="p-[80px]">
       <div className="p-5 flex justify-between">
-        <h1 className="text-[40px]">INVOICE</h1>
-        <h2 className="">Brand Name</h2>
+        <h1 className="text-[80px]">INVOICE</h1>
+        {/* <h2 className="text-[40px]">Brand Name</h2> */}
       </div>
-      <div className="p-5 flex justify-between">
+      <div className="p-5 flex justify-between align-bottom">
         <div className="text-3 font-normal">
-          <p className="text-[15px]">Bill To:</p>
-          <p className="text-[15px] font-semibold">Person Name</p>
-          <p className="text-[12px] font-normal">Email Address</p>
-          <p className="text-[12px] font-normal">Phone Number</p>
-          <p className="text-[12px] font-normal">Nationality</p>
+          <p className="text-[40px]">Bill To:</p>
+          <p className="text-[40px] font-semibold">{data?.name}</p>
+          <p className="text-[25px] font-normal">{data?.email}</p>
+          <p className="text-[25px] font-normal">{data?.phoneNumber}</p>
+          <p className="text-[25px] font-normal">
+            {data?.country?.countryName}
+          </p>
         </div>
-        <div className="text-[12px] font-normal">
-          <p className="text-[15px] font-semibold">Invoice Details</p>
-          <p className="text-[12px] font-normal">
-            <span>Invoice Date</span>
+        <div className="text-[12px] font-normal flex flex-col  justify-end">
+          <p className="text-[40px] font-semibold">Invoice Details</p>
+          <p className="text-[25px] font-normal">
+            <span>Invoice Date: </span>
             <span></span>
           </p>
-          <p className="text-[12px] font-normal">
+          <p className="text-[25px] font-normal">
             <span>Due Date</span>
             <span></span>
           </p>
-          <p className="text-[12px] font-normal">
-            <span>Invoice No</span>
+          <p className="text-[25px] font-normal">
+            <span>Invoice No: {data?.referenceNumber}</span>
             <span></span>
           </p>
         </div>
       </div>
-      <div className="pb-5 ">
-        <div className="table">
+      <div className="pb-5 mt-5 w-[100%] ">
+        <div className="table w-[100%]">
           <table id="invoice" className="w-[100%] border-collapse">
-            <thead>
+            <thead className="text-[30px]">
               <tr className="text-white text-left bg-[#4f4f4f]">
                 <th className="p-2 border-white border-solid border-[1px] py-3 ">
                   Name
@@ -56,51 +59,33 @@ const AttractionInvoicePdfTemplate = () => {
                 </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  Name of Excursion
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  1
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  2
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  0
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  Without
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  1200 INR
-                </td>
-              </tr>
-              <tr>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  Name of Excursion
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  1
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  2
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  0
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  Without
-                </td>
-                <td className="p-2 border-solid border-white border-[1px]">
-                  1200 INR
-                </td>
-              </tr>
+            <tbody className="text-[25px]">
+              {data?.activites?.map((ele) => (
+                <tr>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    {ele?.activity?.name}
+                  </td>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    {ele?.adultsCount}
+                  </td>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    {ele?.childrenCount}
+                  </td>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    {ele?.infantCount}
+                  </td>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    {ele?.transferType}
+                  </td>
+                  <td className="p-2 border-solid border-white border-[1px]">
+                    1200 INR
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-        <div className="pt-2.5 flex justify-end w-[100%]">
+        <div className="pt-2.5 flex justify-end w-[100%] text-[25px] mt-10">
           <div className="grand_total w-[300px]">
             <div className="flex justify-between">
               <p className="">Sub Total</p>
@@ -117,7 +102,7 @@ const AttractionInvoicePdfTemplate = () => {
           </div>
         </div>
       </div>
-      <div className="pt-2.5 flex justify-start ">
+      <div className="pt-2.5 flex justify-start text-[25px]">
         <div className="payment w-[300px]">
           <div className="">
             <h4 className="">Payment Info</h4>
@@ -136,10 +121,10 @@ const AttractionInvoicePdfTemplate = () => {
           </div>
         </div>
       </div>
-      <div className="pt-2.5 flex justify-start">
+      <div className="pt-2.5 flex justify-start text-[25px] mt-10">
         <div className=" flex flex-col">
           <div className=" font-bold">
-            <h4 className="">Terms & Condition</h4>
+            <h4 className="text-[30px]">Terms & Condition</h4>
           </div>
           <p className="">
             By default, Tailwind includes grid-template-column utilities f
