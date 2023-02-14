3d50ea4aef6fc9cbe52314b4d4c45da871bf4aae
nikhil shakya <nikhil.shakya@roado.co.in>
2023-02-14 15:13:56 +0530

    added multiple tickets download in a single pdf file

--- src/b2b/pages/Ticket/AttractionTicketTemplate.jsx
+++ src/b2b/pages/Ticket/AttractionTicketTemplate.jsx
@@ -1,36 +1,45 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useRef } from "react";
 import Barcode from "react-barcode";
 import QRCode from "react-qr-code";
 import formatDate from "../../../utils/formatDate";
 
 const AttractionTicketTemplate = ({ ticket }) => {
-  console.log(ticket);
   const ticketDescRef = useRef();
   let parser = new DOMParser();
 
-  const dom = parser.parseFromString(
-    ticket?.activity?.description,
-    "text/html"
+  const [description, setDescription] = useState(
+    parser
+      .parseFromString(ticket?.activity?.description, "text/html")
+      .getElementsByTagName("body")
+      .item(0)
   );
+
   if (ticketDescRef?.current) {
-    const description = dom.getElementsByTagName("body").item(0);
     ticketDescRef.current.innerHTML = description?.innerHTML;
-    console.log(description);
-    console.log(ticketDescRef.current);
   }
+
+  useEffect(() => {
+    const dom = parser
+      .parseFromString(ticket?.activity?.description, "text/html")
+      .getElementsByTagName("body")
+      .item(0);
+    setDescription(dom);
+  }, []);
 
   const baseUrl = process.env.REACT_APP_SERVER_URL;
 
   return (
     <>
-      <div id="ticket_template" className="w-[100%] min-h-screen p-[40px] ">
+      <div id="ticket_template" className=" p-[40px] w-[1000px] min-h-[1150px]">
         <section className="w-[100%] mx-auto flex flex-col items-center">
           <div className="primary__section w-[90%]">
             <div className="flex justify-between pt-7">
-              <div className="w-[400px] h-[150px] ">
+              <div className="  ">
                 <img
-                  className="w-[400px] h-[150px]"
+                  className="w-[200px] "
                   src={`${baseUrl}${ticket?.attraction?.logo}`}
                   alt=""
                 />
@@ -38,24 +47,24 @@ const AttractionTicketTemplate = ({ ticket }) => {
               <div className="">
                 <Barcode
                   value={ticket?.ticketNo}
-                  width={4}
-                  height={100}
+                  width={2}
+                  height={50}
                   textMargin={0}
                   fontSize={30}
                 />
               </div>
             </div>
           </div>
-          <div className="secondary__section  w-[90%] py-[40px] h-[600px]">
-            <div className="bg-[#e3f2fd] rounded-2xl mt-4 border-2 border-[#a3c4dc] grid grid-cols-12 items-center h-[500px]">
-              <div className="col-span-7 border-r-2 border-dashed border-[#a3c4dc] p-16 h-[450px]">
+          <div className="secondary__section  w-[90%] py-[40px] ">
+            <div className="bg-[#e3f2fd] rounded-2xl mt-4 border-2 border-[#a3c4dc] grid grid-cols-12 items-center h-[250px]">
+              <div className="col-span-7 border-r-2 border-dashed border-[#a3c4dc] p-4 h-[250px] flex flex-col justify-center">
                 <div className="  border-b  border-dashed border-[#a3c4dc] ">
-                  <h1 className="text-[40px] py-2 font-[600]">
+                  <h1 className="text-[20px] py-2 font-[600]">
                     Tour Name : {ticket?.activity?.name}
                   </h1>
                 </div>
-                <div className="grid grid-cols-2 text-[25px] mt-8">
-                  <div className="grid grid-cols-2 gap-x-3 gap-y-16">
+                <div className="grid grid-cols-2 text-[15px] mt-4">
+                  <div className="grid grid-cols-2 gap-x-1 gap-y-2">
                     <div className="">Ticket Type :</div>
                     <div className="capitalize">{ticket?.ticketFor}</div>
                     <div className="">Destination :</div>
@@ -63,7 +72,7 @@ const AttractionTicketTemplate = ({ ticket }) => {
                       {ticket?.destination?.name}
                     </div>
                   </div>
-                  <div className="grid grid-cols-2 gap-x-1 gap-y-16">
+                  <div className="grid grid-cols-2 gap-x-1 gap-y-2">
                     <div className="">Validity Till :</div>
                     <div className="">
                       {ticket?.validity ? formatDate(ticket?.validTill) : "N/A"}
@@ -73,13 +82,13 @@ const AttractionTicketTemplate = ({ ticket }) => {
                   </div>
                 </div>
               </div>
-              <div className="col-span-5 py-10 relative h-[500px]">
-                <div className="h-10 w-10 rounded-full bg-white absolute -top-4 -left-[20px]"></div>
-                <div className="h-10 w-10 rounded-full bg-white absolute -bottom-4 -left-[20px]"></div>
+              <div className="col-span-5 py-10 relative h-[250px]">
+                <div className="h-5 w-5 rounded-full bg-white absolute -top-2 -left-[10px]"></div>
+                <div className="h-5 w-5 rounded-full bg-white absolute -bottom-2 -left-[10px]"></div>
                 <div className="w-full h-full flex justify-center items-center">
                   <div className="">
                     <div className="flex justify-center">
-                      <div className="h-[300px] w-[300px]">
+                      <div className="h-[150px] w-[150px]">
                         <QRCode
                           size={256}
                           style={{
@@ -92,10 +101,10 @@ const AttractionTicketTemplate = ({ ticket }) => {
                         />
                       </div>
                     </div>
-                    <p className="text-[30px] text-center mt-2">
+                    <p className="text-[15px] text-center mt-2">
                       {ticket?.ticketNo}
                     </p>
-                    <p className="text-[30px] text-center">
+                    <p className="text-[15px] text-center">
                       Place Image against the scanner
                     </p>
                   </div>
