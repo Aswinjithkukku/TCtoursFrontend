79fb3b7b31a6cc87916f000acca487189f15106a
aswinjithkukku <aswinjitht5@gmail.com>
2023-02-15 20:13:55 +0530

    index error fixed

--- src/Pages/General/SpecialofferCardSection.jsx
+++ src/Pages/General/SpecialofferCardSection.jsx
@@ -16,19 +16,19 @@ function SpecialofferCardSection() {
                         <div className='  h-[23em] group cursor-pointer'>
                             <div className=' relative '>
                                 <div className='overflow-hidden lg:rounded-xl bg-bottom'>
-                                    <img className='group-hover:scale-110 object-cover  h-[23em] w-full transition-all duration-500 cursor-pointer' src={process.env.REACT_APP_SERVER_URL + home?.cards[0]?.backgroundImage} alt='special offer' />
+                                    <img className='group-hover:scale-110 object-cover  h-[23em] w-full transition-all duration-500 cursor-pointer' src={home && home?.cards?.length > 0 && process.env.REACT_APP_SERVER_URL + home?.cards[0]?.backgroundImage} alt='special offer' />
                                 </div>
                                 <div className='absolute top-10 px-10'>
                                     <div className=' pt-5'>
                                         <span className='bg-main text-light py-[8px] px-3'>
-                                            {home?.cards[0]?.tag}
+                                            {home && home?.cards?.length > 0  && home?.cards[0]?.tag}
                                         </span>
                                     </div>
                                     <div className='text-3xl text-darktext font-medium pt-5'>
-                                        {home?.cards[0]?.title}
+                                        {home && home?.cards?.length > 0  && home?.cards[0]?.title}
                                     </div>
                                     <div className='text-sm text-darktext pt-3'>
-                                        <p className=''>{home?.cards[0]?.description}</p>
+                                        <p className=''>{home && home?.cards?.length > 0 && home?.cards[0]?.description}</p>
                                     </div>
                                     <div className='pt-3 mt-20 scale-0 group-hover:scale-100 group-hover:mt-0 transition-all duration-700'>
                                         <button className='py-2 px-3 border-main border text-main  '>See Deals</button>
