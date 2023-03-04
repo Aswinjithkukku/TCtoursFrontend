055db842c243a59f7069bfe2e7e2a7823e2c870d
aswinjithkukku <aswinjitht5@gmail.com>
2023-03-04 12:08:26 +0530

    b2c attraction

--- src/Pages/Searching/SearchHomePage.jsx
+++ src/Pages/Searching/SearchHomePage.jsx
@@ -1,155 +1,227 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AiOutlineClose, AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import { GiIsland } from "react-icons/gi";
 import { RiCloseLine } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useHandleClickOutside } from "../../hooks";
 import { getCategories } from "../../redux/slices/excursionSlice";
+import { getSearchQuery } from "../../redux/slices/homeSlice";
 
 function SearchHomePage({
-  viewCategory,
-  setViewCategory,
-  setCategory,
-  setSearch,
-  category,
+   viewCategory,
+   setViewCategory,
+   setCategory,
+   setSearch,
+   category,
 }) {
-  const dispatch = useDispatch();
+   const dispatch = useDispatch();
 
-  const [viewFilter, setViewFilter] = useState(false);
-  const [keyword, setKeyword] = useState("");
+   const [viewFilter, setViewFilter] = useState(false);
+   const [keyword, setKeyword] = useState("");
 
-  const { categories } = useSelector((state) => state.excursion);
+   const { categories } = useSelector((state) => state.excursion);
 
-  useEffect(() => {
-    dispatch(getCategories());
-  }, [dispatch]);
+   useEffect(() => {
+      dispatch(getCategories());
+   }, [dispatch]);
 
-  useEffect(() => {
-    setSearch(keyword);
-  }, [keyword]);
+   useEffect(() => {
+      setSearch(keyword);
+   }, [keyword]);
 
-  return (
-    <>
-      <div
-        className={`${
-          viewFilter ? "fixed" : "hidden"
-        }  z-10 bottom-0 left-0 right-0 top-0 lightglass`}
-        onClick={() => setViewFilter(!viewFilter)}
-      ></div>
-      <div className="lg:pb-10 py-5">
-        <div className="lg:max-w-screen-xl lg:mx-auto space-y-7">
-          <form>
-            <div className="mx-5 lg:mx-0 flex">
-              <input
-                type="search"
-                placeholder="Search here!!!"
-                onChange={(e) => setKeyword(e.target.value)}
-                className="px-3 bg-trans w-full border placeholder:text-text py-3 focus:outline-none focus:border-none focus:ring-1 focus:ring-blue rounded-l-md text-darktext"
-              />
-              <button
-                type="submit"
-                className="px-4 text-light bg-lightblue rounded-r-md "
-              >
-                Search
-              </button>
+   const navigate = useNavigate();
+
+   const [value, setValue] = useState("");
+   const [datalist, setDatalist] = useState(false);
+
+   const { searchQuery } = useSelector((state) => state.home);
+
+   const dropdownWrapperRef = useRef();
+   useHandleClickOutside(dropdownWrapperRef, () => setDatalist(false));
+
+   const handleFocus = (e) => {
+      setDatalist(true);
+   };
+
+   useEffect(() => {
+      dispatch(getSearchQuery(value));
+   }, [dispatch, value]);
+
+   return (
+      <>
+         <div
+            className={`${
+               viewFilter ? "fixed" : "hidden"
+            }  z-10 bottom-0 left-0 right-0 top-0 lightglass`}
+            onClick={() => setViewFilter(!viewFilter)}
+         ></div>
+         <div className="lg:pb-10 py-5">
+            <div className="lg:max-w-screen-xl lg:mx-auto space-y-7">
+               <div className="" ref={dropdownWrapperRef}>
+                  <div className="relative">
+                     <input
+                        type="text"
+                        list="Country"
+                        value={value}
+                        placeholder="Where do you want to go?"
+                        onChange={(e) => setValue(e.target.value)}
+                        onFocus={handleFocus}
+                        required
+                        className="capitalize bg-gray-50/40 px-3 w-full border outline-none  placeholder:text-text py-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue rounded-xl text-darktext"
+                     />
+                  </div>
+                  {datalist && (
+                     <div className="absolute max-h-[17em] w-[21em] mt-1  bg-light rounded-lg overflow-y-auto z-20">
+                        <div className="w-full p-2 overflow-y-auto">
+                           <div className="">
+                              <p className="bg-gray-200 py-[2px] px-2 text-[14px] font-[600] text-textColor">
+                                 Destinations
+                              </p>
+                              {searchQuery?.destinations.map((item) => (
+                                 <>
+                                    <div
+                                       key={item?.name}
+                                       className=" py-2 px-2 cursor-pointer capitalize text-darktext z-30 border-b text-sm"
+                                       onClick={() => {
+                                          setValue(item?.name);
+                                          setDatalist(!datalist);
+                                          navigate(`/search/${item?.name}`);
+                                       }}
+                                    >
+                                       {item?.name}
+                                    </div>
+                                 </>
+                              ))}
+                           </div>
+                           <div className="">
+                              <p className="bg-gray-200 py-[2px] px-2 text-[14px] font-[600] text-textColor">
+                                 Attractions
+                              </p>
+                              {searchQuery?.attractions.map((item) => (
+                                 <div
+                                    key={item.title}
+                                    className=" py-2 px-2 cursor-pointer capitalize text-darktext z-30 border-b text-sm"
+                                    onClick={() => {
+                                       setValue(item.title);
+                                       setDatalist(!datalist);
+                                       navigate(`/details/${item?._id}`);
+                                    }}
+                                 >
+                                    {item.title}
+                                 </div>
+                              ))}
+                           </div>
+                        </div>
+                     </div>
+                  )}
+               </div>
+
+               <div
+                  className={`bg-light  p-5 lg:p-0 rounded-t-3xl lg:rounded-none lg:bg-transparent lg:h-auto lg:w-auto max-h-[80vh] w-full fixed lg:static ${
+                     viewFilter ? "bottom-0" : "-bottom-full"
+                  } z-10 transition-all duration-500`}
+               >
+                  <div className="flex lg:hidden justify-between text-darktext p-3">
+                     <span className="text-xl font-semibold">Category</span>
+                     <span
+                        className="text-3xl"
+                        onClick={() => setViewFilter(!viewFilter)}
+                     >
+                        <AiOutlineClose />
+                     </span>
+                  </div>
+                  <div className="relative lg:px-7">
+                     <div
+                        className="hidden absolute left-0 top-5 z-10 h-7 w-7 rounded-full bg-gray-400 lg:flex justify-center items-center text-white"
+                        onClick={() => {
+                           document.querySelector(
+                              ".containerBAL"
+                           ).scrollLeft -= 200;
+                        }}
+                     >
+                        <AiOutlineLeft />
+                     </div>
+                     <div
+                        className="hidden absolute right-0 top-5 z-10 h-7 w-7 rounded-full bg-gray-400 lg:flex justify-center items-center text-white"
+                        onClick={() => {
+                           document.querySelector(
+                              ".containerBAL"
+                           ).scrollLeft += 200;
+                        }}
+                     >
+                        <AiOutlineRight />
+                     </div>
+                     <div
+                        className={`containerBAL scroll-smooth flex overflow-x-auto snap-x scrollbar-hide`}
+                     >
+                        <div className=" justify-between lg:space-x-3 lg:flex w-full  px-1">
+                           {categories?.map((item) => (
+                              <div
+                                 key={item?._id}
+                                 className={`relative my-2 space-x-2 lg:max-w-full lg:min-w-[190px] w-full flex px-3 ${
+                                    category === item?._id
+                                       ? "bg-lightblue"
+                                       : "bg-gray-200"
+                                 }  hover:text-lightblue lg:justify-center items-center py-4 rounded-lg lg:border lg:border-gray-300 cursor-pointer capitalize`}
+                                 onClick={() => setCategory(item?._id)}
+                              >
+                                 {category === item?._id && (
+                                    <span
+                                       className="absolute -top-2 -right-2 text-xl rounded-full w-[17px] h-[17px] flex justify-center items-center bg-darktext"
+                                       onClick={(e) => e.stopPropagation()}
+                                    >
+                                       <p
+                                          className="text-sm text-white"
+                                          onClick={() => setCategory("")}
+                                       >
+                                          <RiCloseLine />{" "}
+                                       </p>
+                                    </span>
+                                 )}
+                                 <img
+                                    src={
+                                       process.env.REACT_APP_SERVER_URL +
+                                       item?.icon
+                                    }
+                                    alt="icon"
+                                    className="w-5 h-5"
+                                 />
+                                 <span
+                                    className={`whitespace-nowrap  font-[500] uppercase text-sm ${
+                                       category === item?._id && "text-white"
+                                    }`}
+                                 >
+                                    {item?.categoryName}
+                                 </span>
+                              </div>
+                           ))}
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className=" flex justify-around lg:hidden">
+                  <span className="">
+                     <button
+                        className="border border-blue-500 rounded-lg px-5 py-2 hover:text-blue"
+                        onClick={() => setViewFilter(!viewFilter)}
+                     >
+                        Category
+                     </button>
+                  </span>
+                  <span className="">
+                     <button
+                        className="border border-blue-500 rounded-lg px-10 py-2"
+                        onClick={() => setViewCategory(!viewCategory)}
+                     >
+                        Filters
+                     </button>
+                  </span>
+               </div>
             </div>
-          </form>
-
-          <div
-            className={`bg-light  p-5 lg:p-0 rounded-t-3xl lg:rounded-none lg:bg-transparent lg:h-auto lg:w-auto max-h-[80vh] w-full fixed lg:static ${
-              viewFilter ? "bottom-0" : "-bottom-full"
-            } z-10 transition-all duration-500`}
-          >
-            <div className="flex lg:hidden justify-between text-darktext p-3">
-              <span className="text-xl font-semibold">Category</span>
-              <span
-                className="text-3xl"
-                onClick={() => setViewFilter(!viewFilter)}
-              >
-                <AiOutlineClose />
-              </span>
-            </div>
-            <div className="relative lg:px-7">
-              <div
-                className="hidden absolute left-0 top-5 z-10 h-7 w-7 rounded-full bg-gray-400 lg:flex justify-center items-center text-white"
-                onClick={() => {
-                  document.querySelector(".containerBAL").scrollLeft -= 200;
-                }}
-              >
-                <AiOutlineLeft />
-              </div>
-              <div
-                className="hidden absolute right-0 top-5 z-10 h-7 w-7 rounded-full bg-gray-400 lg:flex justify-center items-center text-white"
-                onClick={() => {
-                  document.querySelector(".containerBAL").scrollLeft += 200;
-                }}
-              >
-                <AiOutlineRight />
-              </div>
-              <div
-                className={`containerBAL scroll-smooth flex overflow-x-auto snap-x scrollbar-hide`}
-              >
-                <div className=" justify-between lg:space-x-3 lg:flex w-full  px-1">
-                  {categories?.map((item) => (
-                    <div
-                      key={item?._id}
-                      className={`relative my-2 space-x-2 lg:max-w-full lg:min-w-[190px] w-full flex px-3 ${
-                        category === item?._id ? "bg-lightblue" : "bg-gray-200"
-                      }  hover:text-lightblue lg:justify-center items-center py-4 rounded-lg lg:border lg:border-gray-300 cursor-pointer capitalize`}
-                      onClick={() => setCategory(item?._id)}
-                    >
-                      {category === item?._id && (
-                        <span
-                          className="absolute -top-2 -right-2 text-xl rounded-full w-[17px] h-[17px] flex justify-center items-center bg-darktext"
-                          onClick={(e) => e.stopPropagation()}
-                        >
-                          <p
-                            className="text-sm text-white"
-                            onClick={() => setCategory("")}
-                          >
-                            <RiCloseLine />{" "}
-                          </p>
-                        </span>
-                      )}
-                      <img
-                        src={process.env.REACT_APP_SERVER_URL + item?.icon}
-                        alt="icon"
-                        className="w-5 h-5"
-                      />
-                      <span
-                        className={`whitespace-nowrap  font-[500] uppercase text-sm ${
-                          category === item?._id && "text-white"
-                        }`}
-                      >
-                        {item?.categoryName}
-                      </span>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className=" flex justify-around lg:hidden">
-            <span className="">
-              <button
-                className="border border-blue-500 rounded-lg px-5 py-2 hover:text-blue"
-                onClick={() => setViewFilter(!viewFilter)}
-              >
-                Category
-              </button>
-            </span>
-            <span className="">
-              <button
-                className="border border-blue-500 rounded-lg px-10 py-2"
-                onClick={() => setViewCategory(!viewCategory)}
-              >
-                Filters
-              </button>
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+         </div>
+      </>
+   );
 }
 
 export default SearchHomePage;
--- src/components/Cards/VisaCard.jsx
+++ src/components/Cards/VisaCard.jsx
@@ -9,96 +9,88 @@ import { Link } from "react-router-dom";
 import { useHandleClickOutside } from "../../hooks";
 
 function VisaCard() {
-  const [destinations, setDestinations] = useState([]);
-  const [datalist, setDatalist] = useState(false);
-  const [country, setCountry] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-  const dropdownWrapperRef = useRef();
+   const [destinations, setDestinations] = useState([]);
+   const [datalist, setDatalist] = useState(false);
+   const [country, setCountry] = useState("");
+   const [filteredData, setFilteredData] = useState([]);
+   const dropdownWrapperRef = useRef();
 
-  useHandleClickOutside(dropdownWrapperRef, () => setDatalist(false));
+   useHandleClickOutside(dropdownWrapperRef, () => setDatalist(false));
 
-  const handleFocus = (e) => {
-    setDatalist(true);
-  };
+   const handleFocus = (e) => {
+      setDatalist(true);
+   };
 
-  useEffect(() => {
-    const getVisaLIst = async () => {
-      const res = await axios.get("/visa/all");
-      if (res.status === 200) {
-        setDestinations([...res.data]);
-      }
-    };
-    getVisaLIst();
-  }, []);
+   useEffect(() => {
+      const getVisaLIst = async () => {
+         const res = await axios.get("/visa/all");
+         if (res.status === 200) {
+            setDestinations([...res.data]);
+         }
+      };
+      getVisaLIst();
+   }, []);
 
-  useEffect(() => {
-    console.log(destinations);
-    const list = destinations?.filter((data) => {
-      return data.country?.countryName?.toLowerCase().startsWith(country);
-    });
+   useEffect(() => {
+      console.log(destinations);
+      const list = destinations?.filter((data) => {
+         return data.country?.countryName?.toLowerCase().startsWith(country);
+      });
 
-    setFilteredData(list);
-  }, [country, destinations]);
+      setFilteredData(list);
+   }, [country, destinations]);
 
-  console.log(filteredData);
+   console.log(filteredData);
 
-  return (
-    <div className="md:grid md:grid-cols-12 gap-0 py-4 space-y-4 md:space-y-0">
-      <div className="md:col-span-10 flex justify-center items-center">
-        <div className="space-y-2 w-10/12 ">
-          <div className="flex items-center space-x-2 text-darktext">
-            <span className="text-2xl text-blue-500">
-              <IoLocationOutline />{" "}
-            </span>
-            <span className="text-lg text-white">Country</span>
-          </div>
+   return (
+      <div className="py-4 space-y-4 md:space-y-0">
+         <div className=" flex justify-center items-center">
+            <div className="space-y-2 w-10/12 ">
+               <div className="flex items-center space-x-2 text-darktext">
+                  <span className="text-2xl text-blue-500">
+                     <IoLocationOutline />{" "}
+                  </span>
+                  <span className="text-lg text-white">Country</span>
+               </div>
 
-          <div className="" ref={dropdownWrapperRef}>
-            <div className="relative">
-              <input
-                type="text"
-                list="Country"
-                value={country?.country?.countryName}
-                placeholder="Where do you want to go?"
-                onChange={(e) => setCountry(e.target.value)}
-                onFocus={handleFocus}
-                required
-                className="capitalize bg-gray-50/80 px-3 w-full border-none placeholder:text-text py-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue rounded-xl text-darktext"
-              />
+               <div className="" ref={dropdownWrapperRef}>
+                  <div className="relative">
+                     <input
+                        type="text"
+                        list="Country"
+                        value={country?.country?.countryName}
+                        placeholder="Where do you want to go?"
+                        onChange={(e) => setCountry(e.target.value)}
+                        onFocus={handleFocus}
+                        required
+                        className="capitalize bg-gray-50/40 px-3 w-full border-none placeholder:text-text py-3 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue rounded-xl text-darktext"
+                     />
+                  </div>
+                  {datalist && (
+                     <div className="absolute max-h-[17em] w-[21em] mt-1  bg-light rounded-lg overflow-y-auto">
+                        <div className="w-full p-2 overflow-y-auto">
+                           {filteredData?.map((item) => (
+                              <Link to="/visa" state={country?._id}>
+                                 <div
+                                    key={item?._id}
+                                    className="bg-soft py-2 px-2 cursor-pointer capitalize  z-30"
+                                    onClick={() => {
+                                       setCountry(item);
+                                       setDatalist(!datalist);
+                                    }}
+                                 >
+                                    {item?.country?.countryName}
+                                 </div>
+                              </Link>
+                           ))}
+                        </div>
+                     </div>
+                  )}
+               </div>
             </div>
-            {datalist && (
-              <div className="absolute max-h-[17em] w-[21em] mt-1  bg-light rounded-lg overflow-y-auto">
-                <div className="w-full p-2 overflow-y-auto">
-                  {filteredData?.map((item) => (
-                    <div
-                      key={item?._id}
-                      className="bg-soft py-2 px-2 cursor-pointer capitalize  z-30"
-                      onClick={() => {
-                        setCountry(item);
-                        setDatalist(!datalist);
-                      }}
-                    >
-                      {item?.country?.countryName}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
+         </div>
       </div>
-
-      <div className="md:col-span-2 flex justify-center items-center">
-        <div className="">
-          <Link to="/visa" state={country?._id}>
-            <button className="h-14 w-14 bg-blueColor rounded-xl text-light text-3xl flex justify-center items-center">
-              <AiOutlineSearch />
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default VisaCard;
